f791bd4b956be116ff09a6542b227c0218d90edf
RobertByrnes <robbyrnes@hotmail.co.uk>
2022-06-27 17:37:27 +0100

    add config.example.h

---
+++ include/config.example.h
@@ -0,0 +1,6 @@
+// Network
+const char* ssid = "ssid";          // Your WiFi SSID
+const char* password = "password";  // Your WiFi Password
+
+// LED pin
+#define LED_PIN 13
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,46 @@
 #include <Arduino.h>
+#include <config.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <WebSerial.h>
+
+
+AsyncWebServer server(80);
+
+void recvMsg(uint8_t *data, size_t len){
+  WebSerial.println("Received Data...");
+  String d = "";
+  for(int i=0; i < len; i++){
+    d += char(data[i]);
+  }
+  WebSerial.println(d);
+  if (d == "ON"){
+    digitalWrite(LED_PIN, HIGH);
+  }
+  if (d=="OFF"){
+    digitalWrite(LED_PIN, LOW);
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.printf("WiFi Failed!\n");
+    return;
+  }
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  // WebSerial is accessible at "<IP Address>/webserial" in browser
+  WebSerial.begin(&server);
+  WebSerial.msgCallback(recvMsg);
+  server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  WebSerial.println("Hello!");
+  delay(2000);
 }
