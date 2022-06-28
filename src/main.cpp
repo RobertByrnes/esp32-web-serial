07f801cd3c3776fc5a11a361c2885fe34635010b
RobertByrnes <robbyrnes@hotmail.co.uk>
2022-06-28 20:43:42 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,186 @@
 #include <Arduino.h>
 #include <config.h>
-#include <WiFi.h>
-#include <AsyncTCP.h>
-#include <ESPAsyncWebServer.h>
-#include <WebSerial.h>
+#include <Wire.h>
+#include <TinyGsmClient.h>
+#include <PubSubClient.h>
+
+#ifdef DUMP_AT_COMMANDS
+  #include <StreamDebugger.h>
+  StreamDebugger debugger(SerialAT, SerialMon);
+  TinyGsm modem(debugger);
+#else
+  TinyGsm modem(SerialAT);
+#endif
+
+TinyGsmClient client(modem);
+PubSubClient mqtt(client);
 
 
-AsyncWebServer server(80);
 
-void recvMsg(uint8_t *data, size_t len){
-  WebSerial.println("Received Data...");
-  String d = "";
-  for(int i=0; i < len; i++){
-    d += char(data[i]);
+#define OUTPUT_1             2
+#define OUTPUT_2             15
+
+uint32_t lastReconnectAttempt = 0;
+
+#define IP5306_ADDR          0x75
+#define IP5306_REG_SYS_CTL0  0x00
+
+float temperature = 0;
+float humidity = 0;
+long lastMsg = 0;
+
+void mqttCallback(char* topic, byte* message, unsigned int len) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < len; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
   }
-  WebSerial.println(d);
-  if (d == "ON"){
-    digitalWrite(LED_PIN, HIGH);
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp/output1, you check if the message is either "true" or "false". 
+  // Changes the output state according to the message
+  if (String(topic) == "esp/output1") {
+    Serial.print("Changing output to ");
+    if(messageTemp == "true"){
+      Serial.println("true");
+      digitalWrite(OUTPUT_1, HIGH);
+    }
+    else if(messageTemp == "false"){
+      Serial.println("false");
+      digitalWrite(OUTPUT_1, LOW);
+    }
   }
-  if (d=="OFF"){
-    digitalWrite(LED_PIN, LOW);
+  else if (String(topic) == "esp/output2") {
+    Serial.print("Changing output to ");
+    if(messageTemp == "true"){
+      Serial.println("true");
+      digitalWrite(OUTPUT_2, HIGH);
+    }
+    else if(messageTemp == "false"){
+      Serial.println("false");
+      digitalWrite(OUTPUT_2, LOW);
+    }
   }
 }
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(LED_PIN, OUTPUT);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.printf("WiFi Failed!\n");
-    return;
+boolean mqttConnect() {
+  SerialMon.print("Connecting to ");
+  SerialMon.print(broker);
+
+  // Connect to MQTT Broker without username and password
+  //boolean status = mqtt.connect("GsmClientN");
+
+  // Or, if you want to authenticate MQTT:
+  boolean status = mqtt.connect("GsmClientN", mqttUsername, mqttPassword);
+
+  if (status == false) {
+    SerialMon.println(" fail");
+    ESP.restart();
+    return false;
   }
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
-  // WebSerial is accessible at "<IP Address>/webserial" in browser
-  WebSerial.begin(&server);
-  WebSerial.msgCallback(recvMsg);
-  server.begin();
+  SerialMon.println(" success");
+  mqtt.subscribe(topicOutput1);
+  mqtt.subscribe(topicOutput2);
+
+  return mqtt.connected();
+}
+
+
+void setup() {
+  // Set console baud rate
+  SerialMon.begin(115200);
+  delay(10);
+
+  // Set modem reset, enable, power pins
+  pinMode(MODEM_PWKEY, OUTPUT);
+  pinMode(MODEM_RST, OUTPUT);
+  pinMode(MODEM_POWER_ON, OUTPUT);
+  digitalWrite(MODEM_PWKEY, LOW);
+  digitalWrite(MODEM_RST, HIGH);
+  digitalWrite(MODEM_POWER_ON, HIGH);
+  
+  pinMode(OUTPUT_1, OUTPUT);
+  pinMode(OUTPUT_2, OUTPUT);
+  
+  SerialMon.println("Wait...");
+
+  // Set GSM module baud rate and UART pins
+  SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+  delay(6000);
+
+  // Restart takes quite some time
+  // To skip it, call init() instead of restart()
+  SerialMon.println("Initializing modem...");
+  modem.restart();
+  // modem.init();
+
+  String modemInfo = modem.getModemInfo();
+  SerialMon.print("Modem Info: ");
+  SerialMon.println(modemInfo);
+
+  // Unlock your SIM card with a PIN if needed
+  if ( GSM_PIN && modem.getSimStatus() != 3 ) {
+    modem.simUnlock(GSM_PIN);
+  }
+
+  SerialMon.print("Connecting to APN: ");
+  SerialMon.print(apn);
+  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+    SerialMon.println(" fail");
+    ESP.restart();
+  }
+  else {
+    SerialMon.println(" OK");
+  }
+  
+  if (modem.isGprsConnected()) {
+    SerialMon.println("GPRS connected");
+  }
+
+  // MQTT Broker setup
+  mqtt.setServer(broker, 1883);
+  mqtt.setCallback(mqttCallback);
 }
 
 void loop() {
-  WebSerial.println("Hello!");
-  delay(2000);
+  if (!mqtt.connected()) {
+    SerialMon.println("=== MQTT NOT CONNECTED ===");
+    // Reconnect every 10 seconds
+    uint32_t t = millis();
+    if (t - lastReconnectAttempt > 10000L) {
+      lastReconnectAttempt = t;
+      if (mqttConnect()) {
+        lastReconnectAttempt = 0;
+      }
+    }
+    delay(100);
+    return;
+  }
+
+  long now = millis();
+  if (now - lastMsg > 30000) {
+    lastMsg = now;
+    
+    // Convert the value to a char array
+    char tempString[8];
+    dtostrf(temperature, 1, 2, tempString);
+    Serial.print("Temperature: ");
+    Serial.println(tempString);
+    mqtt.publish(topicTemperature, tempString);
+    
+    // Convert the value to a char array
+    char humString[8];
+    dtostrf(humidity, 1, 2, humString);
+    Serial.print("Humidity: ");
+    Serial.println(humString);
+    mqtt.publish(topicHumidity, humString);
+  }
+
+  mqtt.loop();
 }
